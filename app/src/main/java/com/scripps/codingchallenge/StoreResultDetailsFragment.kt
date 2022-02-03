d89a7282ceaaeee00f855e6086cbe7040f7fa364
Eric Versteeg <erversteeg@gmail.com>
2022-02-03 15:20:25 -0500

    renamed second fragment to store results details fragment and condensed the implementation

--- app/src/main/java/com/scripps/codingchallenge/StoreResultDetailsFragment.kt
+++ app/src/main/java/com/scripps/codingchallenge/StoreResultDetailsFragment.kt
@@ -5,28 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.scripps.codingchallenge.databinding.FragmentSecondBinding
+import com.scripps.codingchallenge.databinding.FragmentStoreResultDetailsBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
+class StoreResultDetailsFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentStoreResultDetailsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentStoreResultDetailsBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onDestroyView() {
